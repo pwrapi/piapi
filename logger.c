2f1ab80ac08c8f299b1957b8b9ec0b2de82ab76d
David DeBonis <ddeboni@sandia.gov>
2013-07-15 16:02:35 +0000

    Updated with signal handler for logger
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/kratos@122 a2794be9-68be-4fde-8a1b-ce5afca74b0a

--- logger.c
+++ logger.c
@@ -3,16 +3,26 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <signal.h>
+
+int piapi_sampling;
+
+static void signal_handler(int sig)
+{
+	if( sig == SIGINT )
+		piapi_sampling = 0;		
+}
 
 int main(int argc, char *argv[])
 {
 	void *cntx;
 
+	signal( SIGINT, signal_handler );
 	piapi_init( &cntx, PIAPI_MODE_PROXY, 0x0 ); 
 
+	piapi_sampling = 1;
 	piapi_collect( cntx, PIAPI_PORT_CPU, 0, 100 );
-	while( 1 );
+	while( piapi_sampling );
 
 	piapi_destroy( cntx );
 
