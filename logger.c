6d1fc99c919901fe171dd19a7c677bb5eee630ec
David DeBonis <ddeboni@sandia.gov>
2013-07-17 21:34:01 +0000

    switched logger and proxy
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/kratos@125 a2794be9-68be-4fde-8a1b-ce5afca74b0a

--- logger.c
+++ logger.c
@@ -3,26 +3,64 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
+#include <string.h>
 
 int piapi_sampling;
 
-static void signal_handler(int sig)
+void
+piapi_callback( piapi_sample_t *sample )
 {
-	if( sig == SIGINT )
-		piapi_sampling = 0;		
+        printf( "PIAPI:\n");
+        printf( "\tsample - %u of %u\n", sample->number, sample->total );
+        printf( "\ttime   - %f\n", sample->time_sec+sample->time_usec/1000000.0 );
+        printf( "\tvolts  - %f\n", sample->raw.volts );
+        printf( "\tamps   - %f\n", sample->raw.amps );
+        printf( "\twatts  - %f\n", sample->raw.watts );
+
+	if( sample->number == sample->total ) {
+        	printf( "PIAPI Summary:\n");
+	        printf( "\tavg volts    - %f\n", sample->avg.volts );
+        	printf( "\tavg amps     - %f\n", sample->avg.amps );
+      	  	printf( "\tavg watts    - %f\n", sample->avg.watts );
+
+	        printf( "\tmin volts    - %f\n", sample->min.volts );
+        	printf( "\tmin amps     - %f\n", sample->min.amps );
+      	  	printf( "\tmin watts    - %f\n", sample->min.watts );
+
+	        printf( "\tmax volts    - %f\n", sample->max.volts );
+        	printf( "\tmax amps     - %f\n", sample->max.amps );
+      	  	printf( "\tmax watts    - %f\n", sample->max.watts );
+
+	        printf( "\ttotal time   - %f\n", sample->time_total );
+	        printf( "\ttotal energy - %f\n", sample->energy );
+
+		piapi_sampling = 0;
+	}
 }
 
 int main(int argc, char *argv[])
 {
+	piapi_sample_t sample;
+	unsigned int port;
 	void *cntx;
 
-	signal( SIGINT, signal_handler );
-	piapi_init( &cntx, PIAPI_MODE_PROXY, 0x0, argc, argv ); 
+	piapi_init( &cntx, PIAPI_MODE_PROXY, piapi_callback, argc, argv ); 
 
 	piapi_sampling = 1;
-	piapi_collect( cntx, PIAPI_PORT_CPU, 0, 100 );
-	while( piapi_sampling );
+	piapi_collect( cntx, PIAPI_PORT_CPU, 1000, 100 );
+	while( piapi_sampling);
+
+	for( port = PIAPI_PORT_MIN; port < PIAPI_PORT_MAX; port++ ) {
+		piapi_sampling = 1;
+		piapi_collect( cntx, port, 100, 100 );
+		while( piapi_sampling );
+	}
+
+	piapi_clear( cntx, PIAPI_PORT_CPU );
+	sleep( 2 );
+
+	bzero( &sample, sizeof( piapi_sample_t ) );
+	piapi_counter( cntx, PIAPI_PORT_CPU, &sample);
 
 	piapi_destroy( cntx );
 
--- proxy.c
+++ proxy.c
@@ -3,64 +3,29 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <signal.h>
 
 int piapi_sampling;
 
-void
-piapi_callback( piapi_sample_t *sample )
+static void signal_handler(int sig)
 {
-        printf( "PIAPI:\n");
-        printf( "\tsample - %u of %u\n", sample->number, sample->total );
-        printf( "\ttime   - %f\n", sample->time_sec+sample->time_usec/1000000.0 );
-        printf( "\tvolts  - %f\n", sample->raw.volts );
-        printf( "\tamps   - %f\n", sample->raw.amps );
-        printf( "\twatts  - %f\n", sample->raw.watts );
-
-	if( sample->number == sample->total ) {
-        	printf( "PIAPI Summary:\n");
-	        printf( "\tavg volts    - %f\n", sample->avg.volts );
-        	printf( "\tavg amps     - %f\n", sample->avg.amps );
-      	  	printf( "\tavg watts    - %f\n", sample->avg.watts );
-
-	        printf( "\tmin volts    - %f\n", sample->min.volts );
-        	printf( "\tmin amps     - %f\n", sample->min.amps );
-      	  	printf( "\tmin watts    - %f\n", sample->min.watts );
-
-	        printf( "\tmax volts    - %f\n", sample->max.volts );
-        	printf( "\tmax amps     - %f\n", sample->max.amps );
-      	  	printf( "\tmax watts    - %f\n", sample->max.watts );
-
-	        printf( "\ttotal time   - %f\n", sample->time_total );
-	        printf( "\ttotal energy - %f\n", sample->energy );
-
-		piapi_sampling = 0;
-	}
+	if( sig == SIGINT )
+		piapi_sampling = 0;		
 }
 
 int main(int argc, char *argv[])
 {
-	piapi_sample_t sample;
-	unsigned int port;
-	void *cntx;
+	void *cntx[256];
 
-	piapi_init( &cntx, PIAPI_MODE_PROXY, piapi_callback, argc, argv ); 
+	signal( SIGINT, signal_handler );
 
 	piapi_sampling = 1;
-	piapi_collect( cntx, PIAPI_PORT_CPU, 1000, 100 );
-	while( piapi_sampling);
+	for(unsigned int i = lowerbound; i <= upperbound; i++) {
+		piapi_init( &cntx, PIAPI_MODE_PROXY, 0x0, argc, argv ); 
 
-	for( port = PIAPI_PORT_MIN; port < PIAPI_PORT_MAX; port++ ) {
-		piapi_sampling = 1;
-		piapi_collect( cntx, port, 100, 100 );
-		while( piapi_sampling );
+		piapi_collect( cntx, PIAPI_PORT_CPU, 0, 100 );
 	}
-
-	piapi_clear( cntx, PIAPI_PORT_CPU );
-	sleep( 2 );
-
-	bzero( &sample, sizeof( piapi_sample_t ) );
-	piapi_counter( cntx, PIAPI_PORT_CPU, &sample);
+	while( piapi_sampling );
 
 	piapi_destroy( cntx );
 
