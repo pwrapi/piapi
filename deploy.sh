f0d54d21c27c2af023dc9fd8f48afa01ca9c64ca
David DeBonis <ddeboni@sandia.gov>
2014-03-24 17:57:07 +0000

    updated deploy script
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/kratos@223 a2794be9-68be-4fde-8a1b-ce5afca74b0a

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-IMAGE_PATH=/tftpboot/images/compute.x86_64/usr
+IMAGE_PATH=/tftpboot/images/compute_powerinsight/usr
+#IMAGE_PATH=/tftpboot/images/compute.x86_64/usr
+#IMAGE_PATH=/usr
 
 echo "Copying include files to $IMAGE_PATH"
 cp ./piapi.h $IMAGE_PATH/include
 cp ./picommon.h $IMAGE_PATH/include
 
-echo "Copying library to $IMAGE_PATH"
+echo "Copying libraries to $IMAGE_PATH"
 cp ./libpiapi.so $IMAGE_PATH/lib
+cp ./libpiapi.so $IMAGE_PATH/lib64
 
 echo "Copying executables to $IMAGE_PATH"
 cp ./piproxy $IMAGE_PATH/bin
