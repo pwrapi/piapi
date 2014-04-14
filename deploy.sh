b001b91b16ac09dd62d5f6a2041944966ba32499
David DeBonis <ddeboni@sandia.gov>
2014-04-14 17:39:38 +0000

    updated deploy script
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/kratos@242 a2794be9-68be-4fde-8a1b-ce5afca74b0a

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
-IMAGE_PATH=/tftpboot/images/compute_powerinsight/usr
-#IMAGE_PATH=/tftpboot/images/compute.x86_64/usr
-#IMAGE_PATH=/usr
+for IMAGE_PATH in '/tftpboot/images/compute_powerinsight/usr' '/tftpboot/images/compute.x86_64/usr' '/usr'
+do
+	echo "Copying include files to $IMAGE_PATH"
+	cp ./piapi.h $IMAGE_PATH/include
+	cp ./picommon.h $IMAGE_PATH/include
 
-echo "Copying include files to $IMAGE_PATH"
-cp ./piapi.h $IMAGE_PATH/include
-cp ./picommon.h $IMAGE_PATH/include
+	echo "Copying libraries to $IMAGE_PATH"
+	cp ./libpiapi.so $IMAGE_PATH/lib
+	cp ./libpiapi.so $IMAGE_PATH/lib64
 
-echo "Copying libraries to $IMAGE_PATH"
-cp ./libpiapi.so $IMAGE_PATH/lib
-cp ./libpiapi.so $IMAGE_PATH/lib64
-
-echo "Copying executables to $IMAGE_PATH"
-cp ./piproxy $IMAGE_PATH/bin
-cp ./pilogger $IMAGE_PATH/bin
-
+	echo "Copying executables to $IMAGE_PATH"
+	cp ./piproxy $IMAGE_PATH/bin
+	cp ./pilogger $IMAGE_PATH/bin
+done
