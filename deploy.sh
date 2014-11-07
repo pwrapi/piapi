f6dcb7e1c44585265f83c28625255babc893fbdb
David DeBonis <ddeboni@sandia.gov>
2014-11-07 16:07:24 +0000

    finalized deploy script
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/kratos@296 a2794be9-68be-4fde-8a1b-ce5afca74b0a

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,17 @@ for IMAGE_PATH in '/tftpboot/images/ro-rootfs/home' '/tftpboot/images/ro-rootfs-
 do
 	echo "Extracting beaglebone files to $IMAGE_PATH"
 	rm -rf $IMAGE_PATH/power
-	tar zxpf /home/ddeboni/archive/kratos_tools_arm.tgz $IMAGE_PATH
-	mv $IMAGE_PATH/build $IMAGE_PATH/power
+	tar zxpf /home/ddeboni/archive/kratos_tools_arm.tgz
+
+	echo "Linking configuration file to $IMAGE_PATH"
+	if [ "$IMAGE_PATH" == "/tftpboot/images/ro-rootfs/home" ]
+	then
+		ln -s config.amd build/bin/.config
+	else
+		ln -s config.intel build/bin/.config
+	fi
+
+	echo "Moving beaglebone files to $IMAGE_PATH"
+	mv build $IMAGE_PATH/power
 done
 
