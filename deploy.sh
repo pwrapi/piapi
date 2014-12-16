2a16cf6598cda709ab3741e9a1e64a90236fe5b3
David DeBonis <ddeboni@sandia.gov>
2014-12-16 23:23:39 +0000

    removed powerinsight.conf placement
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/kratos@304 a2794be9-68be-4fde-8a1b-ce5afca74b0a

--- deploy.sh
+++ deploy.sh
@@ -21,16 +21,6 @@ do
 	rm -rf $IMAGE_PATH/power
 	tar zxpf /home/ddeboni/archive/kratos_tools_arm.tgz
 
-	echo "Linking configuration file to $IMAGE_PATH"
-	if [ "$IMAGE_PATH" == "/tftpboot/images/ro-rootfs/home" ]
-	then
-		mv config.amd $IMAGE_PATH/../etc/powerinsight.conf
-		rm config.intel
-	else
-		mv config.intel $IMAGE_PATH/../etc/powerinsight.conf
-		rm config.amd
-	fi
-
 	echo "Moving beaglebone files to $IMAGE_PATH"
 	mv build $IMAGE_PATH/power
 done
