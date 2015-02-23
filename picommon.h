a9bfde1672fc36d97b4b249f11aa2ad4985cbd89
David DeBonis <ddeboni@sandia.gov>
2015-02-23 19:55:08 +0000

    simple tab fix
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/kratos@333 a2794be9-68be-4fde-8a1b-ce5afca74b0a

--- picommon.h
+++ picommon.h
@@ -18,22 +18,22 @@ extern "C" {
 #endif
 
 typedef enum {
-	PIAPI_PORT_UNKNOWN = -1,
-        PIAPI_PORT_HALF = 0,
-        PIAPI_PORT_CPU = 1,
-        PIAPI_PORT_R1_12V = 1,
-        PIAPI_PORT_12V = 2,
-        PIAPI_PORT_R1_3_3V = 2,
-        PIAPI_PORT_MEM = 3,
-        PIAPI_PORT_R2_12V = 3,
-        PIAPI_PORT_5V = 4,
-        PIAPI_PORT_R2_3_3V = 4,
-        PIAPI_PORT_3_3V = 5,
-        PIAPI_PORT_ATX_12V = 5,
-        PIAPI_PORT_HDD_12V = 6,
-        PIAPI_PORT_R3_12V = 6,
-        PIAPI_PORT_HDD_5V = 7,
-        PIAPI_PORT_R3_3_3V = 7,
+    PIAPI_PORT_UNKNOWN = -1,
+    PIAPI_PORT_HALF = 0,
+    PIAPI_PORT_CPU = 1,
+    PIAPI_PORT_R1_12V = 1,
+    PIAPI_PORT_12V = 2,
+    PIAPI_PORT_R1_3_3V = 2,
+    PIAPI_PORT_MEM = 3,
+    PIAPI_PORT_R2_12V = 3,
+    PIAPI_PORT_5V = 4,
+    PIAPI_PORT_R2_3_3V = 4,
+    PIAPI_PORT_3_3V = 5,
+    PIAPI_PORT_ATX_12V = 5,
+    PIAPI_PORT_HDD_12V = 6,
+    PIAPI_PORT_R3_12V = 6,
+    PIAPI_PORT_HDD_5V = 7,
+    PIAPI_PORT_R3_3_3V = 7,
 	PIAPI_PORT_R4_12V = 8,
 	PIAPI_PORT_R4_3_3V = 9,
 	PIAPI_PORT_C1_CPU_12V = 10,
@@ -63,17 +63,17 @@ typedef struct piapi_reading {
 
 typedef struct piapi_sample {
 	void *cntx;
-        unsigned int number;
-        unsigned int total;
-        unsigned long time_sec;
-        unsigned long time_usec;
-        unsigned int port;
+    unsigned int number;
+    unsigned int total;
+    unsigned long time_sec;
+    unsigned long time_usec;
+    unsigned int port;
 	piapi_reading_t raw;
-        piapi_reading_t min;
-        piapi_reading_t max;
-        piapi_reading_t avg;
+    piapi_reading_t min;
+    piapi_reading_t max;
+    piapi_reading_t avg;
 	double time_total;
-        double energy;
+    double energy;
 } piapi_sample_t;
 
 typedef struct piapi_version {
