a7228b007ce3967d5f76e351425f1ff96d37d8b5
David DeBonis <ddeboni@sandia.gov>
2015-03-10 20:07:23 +0000

    updated to 2.0.1
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/kratos@344 a2794be9-68be-4fde-8a1b-ce5afca74b0a

--- picommon.h
+++ picommon.h
@@ -8,9 +8,9 @@
 #ifndef PICOMMON_H
 #define PICOMMON_H
 
-#define PIAPI_MAJOR 1
-#define PIAPI_MINOR 5
-#define PIAPI_BUILD 3
+#define PIAPI_MAJOR 2
+#define PIAPI_MINOR 0
+#define PIAPI_BUILD 1
 #define PIAPI_REV_STR "$Rev$"
 
 #ifdef __cplusplus
