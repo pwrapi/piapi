d5ecfbd994fbaf33e6804144b2f871dbdf1335a3
David DeBonis <ddeboni@sandia.gov>
2015-03-25 19:35:23 +0000

    adding new deployment script for shepard
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/kratos@358 a2794be9-68be-4fde-8a1b-ce5afca74b0a

---
+++ tools/deploy-shepard.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+for IMAGE_PATH in '/images/compute_powerinsight/usr' '/images/compute.x86_64/usr' '/usr'
+do
+	echo "Copying include files to $IMAGE_PATH"
+	cp ./piapi.h $IMAGE_PATH/include
+	cp ./picommon.h $IMAGE_PATH/include
+
+	echo "Copying libraries to $IMAGE_PATH"
+	cp ./libpiapi.so $IMAGE_PATH/lib
+	cp ./libpiapi.so $IMAGE_PATH/lib64
+
+	echo "Copying executables to $IMAGE_PATH"
+	cp ./piproxy $IMAGE_PATH/bin
+	cp ./pilogger $IMAGE_PATH/bin
+done
+
+for IMAGE_PATH in '/images/ro-rootfs/home' '/images/ro-rootfs-alt/home'
+do
+	echo "Extracting beaglebone files to $IMAGE_PATH"
+	rm -rf $IMAGE_PATH/power
+	tar zxpf /home/ddeboni/archive/kratos_tools_arm.tgz
+
+	echo "Moving beaglebone files to $IMAGE_PATH"
+	mv install $IMAGE_PATH/power
+done
+
